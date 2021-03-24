9c253590100c37ec0a66b305cf9d2a45a37d617a
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-03-24 16:24:17 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
