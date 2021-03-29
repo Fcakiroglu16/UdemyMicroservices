fa6ea7b75ae13c70c48161e0a2825b69de5d512f
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-03-29 22:27:44 +0300

    DiscountsController

---
+++ Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,72 @@
+﻿using FreeCourse.Services.Discount.Services;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : CustomBaseController
+    {
+        private readonly IDiscountService _discountService;
+
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public DiscountsController(IDiscountService discountService, ISharedIdentityService sharedIdentityService)
+        {
+            _discountService = discountService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return CreateActionResultInstance(await _discountService.GetAll());
+        }
+
+        //api/discounts/4
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var discount = await _discountService.GetById(id);
+
+            return CreateActionResultInstance(discount);
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/[action]/{code}")]
+        public async Task<IActionResult> GetByCode(string code)
+
+        {
+            var userId = _sharedIdentityService.GetUserId;
+
+            var discount = await _discountService.GetByCodeAndUserId(code, userId);
+
+            return CreateActionResultInstance(discount);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Models.Discount discount)
+        {
+            return CreateActionResultInstance(await _discountService.Save(discount));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Models.Discount discount)
+        {
+            return CreateActionResultInstance(await _discountService.Update(discount));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return CreateActionResultInstance(await _discountService.Delete(id));
+        }
+    }
+}
--- Services/Discount/FreeCourse.Services.Discount/Startup.cs
+++ Services/Discount/FreeCourse.Services.Discount/Startup.cs
@@ -1,3 +1,5 @@
+using FreeCourse.Services.Discount.Services;
+using FreeCourse.Shared.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
@@ -29,6 +31,9 @@ namespace FreeCourse.Services.Discount
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+            services.AddScoped<IDiscountService, DiscountService>();
             var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
