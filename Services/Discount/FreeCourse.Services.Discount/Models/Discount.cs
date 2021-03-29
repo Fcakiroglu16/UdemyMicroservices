5923fa284ef4238c75fa43d589c7032ad7e8912e
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-03-29 20:10:18 +0300

    IDiscountService

---
+++ Services/Discount/FreeCourse.Services.Discount/Models/Discount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Models
+{
+    [Dapper.Contrib.Extensions.Table("discount")]
+    public class Discount
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
---
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,23 @@
+﻿using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<Models.Discount>>> GetAll();
+
+        Task<Response<Models.Discount>> GetById(int id);
+
+        Task<Response<NoContent>> Save(Models.Discount discount);
+
+        Task<Response<NoContent>> Update(Models.Discount discount);
+
+        Task<Response<NoContent>> Delete(int id);
+
+        Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId);
+    }
+}
