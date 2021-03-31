e1d0839e752f8b61dd14720797643867f2313911
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-03-31 22:08:14 +0300

    Query / QueryHandler

---
+++ Services/Order/FreeCourse.Services.Order.Application/Handlers/GetOrdersByUserIdQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Application.Mapping;
+using FreeCourse.Services.Order.Application.Queries;
+using FreeCourse.Services.Order.Infrastructure;
+using FreeCourse.Shared.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Handlers
+{
+    internal class GetOrdersByUserIdQueryHandler : IRequestHandler<GetOrdersByUserIdQuery, Response<List<OrderDto>>>
+    {
+        private readonly OrderDbContext _context;
+
+        public GetOrdersByUserIdQueryHandler(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<List<OrderDto>>> Handle(GetOrdersByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _context.Orders.Include(x => x.OrderItems).Where(x => x.BuyerId == request.UserId).ToListAsync();
+
+            if (!orders.Any())
+            {
+                return Response<List<OrderDto>>.Success(new List<OrderDto>(), 200);
+            }
+
+            var ordersDto = ObjectMapper.Mapper.Map<List<OrderDto>>(orders);
+
+            return Response<List<OrderDto>>.Success(ordersDto, 200);
+        }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Queries
+{
+    internal class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
