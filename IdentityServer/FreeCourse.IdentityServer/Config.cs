8bb8f94a5cad7db2e6fa805effd4033975093c02
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-01 00:18:04 +0300

    Order Microservice, koruma altına alma

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -16,6 +16,8 @@ namespace FreeCourse.IdentityServer
                new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
                   new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
                               new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
+
+                                          new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
                new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -37,7 +39,7 @@ namespace FreeCourse.IdentityServer
 
                      new ApiScope("basket_fullpermission","Basket API için full erişim"),
                             new ApiScope("discount_fullpermission","Discount API için full erişim"),
-
+                             new ApiScope("order_fullpermission","Order API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -59,7 +61,7 @@ namespace FreeCourse.IdentityServer
                     AllowOfflineAccess=true,
                     ClientSecrets= {new Secret("secret".Sha256())},
                     AllowedGrantTypes= GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes={ "basket_fullpermission","discount_fullpermission",  IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
+                    AllowedScopes={ "basket_fullpermission","discount_fullpermission", "order_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName,"roles" },
                     AccessTokenLifetime=1*60*60,
                     RefreshTokenExpiration=TokenExpiration.Absolute,
                     AbsoluteRefreshTokenLifetime= (int) (DateTime.Now.AddDays(60)- DateTime.Now).TotalSeconds,
--- Services/Order/FreeCourse.Services.Order.API/Startup.cs
+++ Services/Order/FreeCourse.Services.Order.API/Startup.cs
@@ -1,9 +1,12 @@
 using FreeCourse.Services.Order.Infrastructure;
 using FreeCourse.Shared.Services;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +15,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -29,6 +33,15 @@ namespace FreeCourse.Services.Order.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_order";
+                options.RequireHttpsMetadata = false;
+            });
+
             services.AddDbContext<OrderDbContext>(opt =>
             {
                 opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), configure =>
@@ -41,7 +54,11 @@ namespace FreeCourse.Services.Order.API
             services.AddScoped<ISharedIdentityService, SharedIdentityService>();
 
             services.AddMediatR(typeof(Application.Handlers.CreateOrderCommandHandler).Assembly);
-            services.AddControllers();
+
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Order.API", Version = "v1" });
@@ -60,6 +77,7 @@ namespace FreeCourse.Services.Order.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
