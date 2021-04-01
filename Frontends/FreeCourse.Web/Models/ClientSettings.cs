0ed40e1a30a7726f262ae95b853b3a2e8ba2fda9
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-02 01:38:06 +0300

    ClientSettings

---
+++ Frontends/FreeCourse.Web/Models/ClientSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models
+{
+    public class ClientSettings
+    {
+        public Client WebClient { get; set; }
+        public Client WebClientForUser { get; set; }
+    }
+
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -23,6 +23,7 @@ namespace FreeCourse.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
             services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
             services.AddControllersWithViews();
         }
