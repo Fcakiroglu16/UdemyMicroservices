6950f352ae1eb4004307c381b10a191d3047435a
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-02 03:08:09 +0300

    AuthController / SignIn.cshtml

---
+++ Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
