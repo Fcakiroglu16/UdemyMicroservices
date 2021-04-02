d67ec764e3a6d386735c49398d3980c44a320c9f
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-02 05:10:45 +0300

    IUserService/UserService-2

--- Frontends/FreeCourse.Web/Services/UserService.cs
+++ Frontends/FreeCourse.Web/Services/UserService.cs
@@ -3,15 +3,24 @@ using FreeCourse.Web.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services
 {
     public class UserService : IUserService
     {
-        public Task<UserViewModel> GetUser()
+        private readonly HttpClient _client;
+
+        public UserService(HttpClient client)
         {
-            throw new NotImplementedException();
+            _client = client;
+        }
+
+        public async Task<UserViewModel> GetUser()
+        {
+            return await _client.GetFromJsonAsync<UserViewModel>("/api/user/getuser");
         }
     }
 }
