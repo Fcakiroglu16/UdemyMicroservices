5b64347b04c3b2c88eb50a961be973bfb33bd806
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-03 18:51:27 +0300

    Logout action method

--- Frontends/FreeCourse.Web/Controllers/AuthController.cs
+++ Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using FreeCourse.Web.Models;
 using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -43,6 +45,13 @@ namespace FreeCourse.Web.Controllers
             }
 
             return RedirectToAction(nameof(Index), "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await _identityService.RevokeRefreshToken();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
