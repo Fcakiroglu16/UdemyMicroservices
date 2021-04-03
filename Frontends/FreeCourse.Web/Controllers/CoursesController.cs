a28b2b92112c69f3f777a8bf689db2236acf098b
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-04 01:37:26 +0300

    CoursesController/Create-2

--- Frontends/FreeCourse.Web/Controllers/CoursesController.cs
+++ Frontends/FreeCourse.Web/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.Shared.Services;
+using FreeCourse.Web.Models.Catalogs;
 using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +35,22 @@ namespace FreeCourse.Web.Controllers
             ViewBag.categoryList = new SelectList(categories, "Id", "Name");
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateInput courseCreateInput)
+        {
+            var categories = await _catalogService.GetAllCategoryAsync();
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name");
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            courseCreateInput.UserId = _sharedIdentityService.GetUserId;
+
+            await _catalogService.CreateCourseAsync(courseCreateInput);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
