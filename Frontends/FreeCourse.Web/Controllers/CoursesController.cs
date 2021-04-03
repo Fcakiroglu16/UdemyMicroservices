577136ee60e564561b3324a2b0d92e22062ef83c
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-04 02:36:46 +0300

    CoursesController/Delete

--- Frontends/FreeCourse.Web/Controllers/CoursesController.cs
+++ Frontends/FreeCourse.Web/Controllers/CoursesController.cs
@@ -92,5 +92,12 @@ namespace FreeCourse.Web.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _catalogService.DeleteCourseAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
