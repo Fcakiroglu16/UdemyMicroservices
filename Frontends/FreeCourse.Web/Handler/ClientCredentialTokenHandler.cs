0455d79d87592cbc654b2d6b2004e751101608dc
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-03 21:25:05 +0300

    ClientCredentialTokenHandler

---
+++ Frontends/FreeCourse.Web/Handler/ClientCredentialTokenHandler.cs
@@ -0,0 +1,36 @@
+﻿using FreeCourse.Web.Exceptions;
+using FreeCourse.Web.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Handler
+{
+    public class ClientCredentialTokenHandler : DelegatingHandler
+    {
+        private readonly IClientCredentialTokenService _clientCredentialTokenService;
+
+        public ClientCredentialTokenHandler(IClientCredentialTokenService clientCredentialTokenService)
+        {
+            _clientCredentialTokenService = clientCredentialTokenService;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await _clientCredentialTokenService.GetToken());
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                throw new UnAuthorizeException();
+            }
+
+            return response;
+        }
+    }
+}
--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -37,13 +37,14 @@ namespace FreeCourse.Web
             services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
 
             services.AddScoped<ResourceOwnerPasswordTokenHandler>();
+            services.AddScoped<ClientCredentialTokenHandler>();
             services.AddHttpClient<IIdentityService, IdentityService>();
 
             services.AddHttpClient<ICatalogService, CatalogService>(opt =>
 
              {
                  opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Catalog.Path}");
-             });
+             }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
 
             services.AddHttpClient<IUserService, UserService>(opt =>
             {
