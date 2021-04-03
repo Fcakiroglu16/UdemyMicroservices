b40a4dc6197bad8e6fd6f449e932c477b9182671
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-04 01:58:21 +0300

    CoursesController/Update-1

--- Frontends/FreeCourse.Web/Controllers/CoursesController.cs
+++ Frontends/FreeCourse.Web/Controllers/CoursesController.cs
@@ -52,5 +52,31 @@ namespace FreeCourse.Web.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            var course = await _catalogService.GetByCourseId(id);
+            var categories = await _catalogService.GetAllCategoryAsync();
+
+            if (course == null)
+            {
+                //mesaj göster
+                RedirectToAction(nameof(Index));
+            }
+            ViewBag.categoryList = new SelectList(categories, "Id", "Name", course.Id);
+            CourseUpdateInput courseUpdateInput = new()
+            {
+                Id = course.Id,
+                Name = course.Name,
+                Description = course.Description,
+                Price = course.Price,
+                Feature = course.Feature,
+                CategoryId = course.CategoryId,
+                UserId = course.UserId,
+                Picture = course.Picture
+            };
+
+            return View(courseUpdateInput);
+        }
     }
 }
