7a4bf4260a8b13c66aaf5e31bf7c203d0ebd3edc
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-04 06:01:45 +0300

    HomeController/Index(Tüm kursları göstermek

--- Frontends/FreeCourse.Web/Controllers/HomeController.cs
+++ Frontends/FreeCourse.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,19 +14,17 @@ namespace FreeCourse.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ICatalogService _catalogService;
+
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return View(await _catalogService.GetAllCourseAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Frontends/FreeCourse.Web/Models/Catalogs/CourseViewModel.cs
+++ Frontends/FreeCourse.Web/Models/Catalogs/CourseViewModel.cs
@@ -14,6 +14,11 @@ namespace FreeCourse.Web.Models.Catalogs
 
         public string Description { get; set; }
 
+        public string ShortDescription
+        {
+            get => Description.Length > 100 ? Description.Substring(0, 100) + "..." : Description;
+        }
+
         public decimal Price { get; set; }
 
         public string UserId { get; set; }
