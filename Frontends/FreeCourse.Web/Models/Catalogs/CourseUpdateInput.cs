ac2498fcd22a88191f46a6ffdba7036b9f931553
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-04 05:16:19 +0300

    CoursesController/Update(Kurs fotografı yüklemek için)

--- Frontends/FreeCourse.Web/Models/Catalogs/CourseUpdateInput.cs
+++ Frontends/FreeCourse.Web/Models/Catalogs/CourseUpdateInput.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +11,16 @@ namespace FreeCourse.Web.Models.Catalogs
     {
         public string Id { get; set; }
 
+        [Display(Name = "Kurs ismi")]
+        [Required]
         public string Name { get; set; }
 
+        [Display(Name = "Kurs açıklama")]
+        [Required]
         public string Description { get; set; }
 
+        [Display(Name = "Kurs fiyat")]
+        [Required]
         public decimal Price { get; set; }
 
         public string UserId { get; set; }
@@ -20,6 +28,11 @@ namespace FreeCourse.Web.Models.Catalogs
         public string Picture { get; set; }
         public FeatureViewModel Feature { get; set; }
 
+        [Display(Name = "Kurs kategori")]
+        [Required]
         public string CategoryId { get; set; }
+
+        [Display(Name = "Kurs Resim")]
+        public IFormFile PhotoFormFile { get; set; }
     }
 }
--- Frontends/FreeCourse.Web/Services/CatalogService.cs
+++ Frontends/FreeCourse.Web/Services/CatalogService.cs
@@ -115,6 +115,14 @@ namespace FreeCourse.Web.Services
 
         public async Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput)
         {
+            var resultPhotoService = await _photoStockService.UploadPhoto(courseUpdateInput.PhotoFormFile);
+
+            if (resultPhotoService != null)
+            {
+                await _photoStockService.DeletePhoto(courseUpdateInput.Picture);
+                courseUpdateInput.Picture = resultPhotoService.Url;
+            }
+
             var response = await _client.PutAsJsonAsync<CourseUpdateInput>("courses", courseUpdateInput);
 
             return response.IsSuccessStatusCode;
