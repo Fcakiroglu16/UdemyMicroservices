94c844e366568e1aa15b9e5252448d1cb4aa3221
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-05 23:46:34 +0300

    IBasketService

---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,25 @@
+﻿using FreeCourse.Web.Models.Baskets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<bool> SaveOrUpdate(BasketViewModel basketViewModel);
+
+        Task<BasketViewModel> Get();
+
+        Task<bool> Delete();
+
+        Task AddBasketItem(BasketItemViewModel basketItemViewModel);
+
+        Task<bool> RemoveBasketItem(string courseId);
+
+        Task<bool> ApplyDiscount(string discountCode);
+
+        Task<bool> CancelApplyDiscount();
+    }
+}
