3aaa3a1e4a847c7cbee5ed4b836e19eff80fd4c9
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-05 22:37:00 +0300

    Basket microservice için viewmodel oluşturma

---
+++ Frontends/FreeCourse.Web/Models/Baskets/BasketItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models.Baskets
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public decimal Price { get; set; }
+
+        private decimal? DiscountAppliedPrice { get; set; }
+
+        public decimal GetCurrentPrice
+        {
+            get => DiscountAppliedPrice != null ? DiscountAppliedPrice.Value : Price;
+        }
+
+        public void AppliedDiscount(decimal discountPrice)
+        {
+            DiscountAppliedPrice = discountPrice;
+        }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Models/Baskets/BasketViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models.Baskets
+{
+    public class BasketViewModel
+    {
+        public string UserId { get; set; }
+
+        public string DiscountCode { get; set; }
+
+        public int? DiscountRate { get; set; }
+        private List<BasketItemViewModel> _basketItems { get; set; }
+
+        public List<BasketItemViewModel> BasketItems
+        {
+            get
+            {
+                if (HasDiscount)
+                {
+                    //Örnek kurs fiyat 100 TL indirim %10
+                    _basketItems.ForEach(x =>
+                    {
+                        var discountPrice = x.Price * ((decimal)DiscountRate.Value / 100);
+                        x.AppliedDiscount(Math.Round(x.Price - discountPrice, 2));
+                    });
+                }
+                return _basketItems;
+            }
+            set
+            {
+                _basketItems = value;
+            }
+        }
+
+        public decimal TotalPrice
+        {
+            get => _basketItems.Sum(x => x.GetCurrentPrice);
+        }
+
+        public bool HasDiscount
+        {
+            get => !string.IsNullOrEmpty(DiscountCode);
+        }
+    }
+}
