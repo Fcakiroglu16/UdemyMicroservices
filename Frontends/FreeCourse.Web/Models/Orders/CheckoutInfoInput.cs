2d43b9b4d9901dde2ac611838607e82dd0df6624
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-07 02:53:08 +0300

    Order Microservice için CheckoutInfoInput

---
+++ Frontends/FreeCourse.Web/Models/Orders/CheckoutInfoInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models.Orders
+{
+    public class CheckoutInfoInput
+    {
+        [Display(Name = "il")]
+        public string Province { get; set; }
+
+        [Display(Name = "İlçe")]
+        public string District { get; set; }
+
+        [Display(Name = "Cadde")]
+        public string Street { get; set; }
+
+        [Display(Name = "Posta Kodu")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "adres")]
+        public string Line { get; set; }
+
+        [Display(Name = "Kart isim soy isim")]
+        public string CardName { get; set; }
+
+        [Display(Name = "Kart numarası")]
+        public string CardNumber { get; set; }
+
+        [Display(Name = "son kullanma tarih(ay/yıl)")]
+        public string Expiration { get; set; }
+
+        [Display(Name = "CVV/CVC2 numarası")]
+        public string CVV { get; set; }
+    }
+}
