2458cea2e082a67c605923175538684c23ae5254
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-07 04:55:26 +0300

    OrderController /Checkout.cshtml

---
+++ Frontends/FreeCourse.Web/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using FreeCourse.Web.Models.Orders;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+
+        public OrderController(IBasketService basketService, IOrderService orderService)
+        {
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        public async Task<IActionResult> Checkout()
+        {
+            var basket = await _basketService.Get();
+
+            ViewBag.basket = basket;
+            return View(new CheckoutInfoInput());
+        }
+    }
+}
