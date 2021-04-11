51174e6fcab89b7ae97b83a71292ae5103c5a8fe
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-11 16:39:28 +0300

    CreateOrderMessageCommand

---
+++ Shared/FreeCourse.Shared/Messages/CreateOrderMessageCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.Messages
+{
+    public class CreateOrderMessageCommand
+    {
+        public string BuyerId { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+
+        public string Province { get; set; }
+
+        public string District { get; set; }
+
+        public string Street { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Line { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
