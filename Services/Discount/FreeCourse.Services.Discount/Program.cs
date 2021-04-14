4727a649c8f556b5c312a8465bb7c48a626204af
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-04-15 02:14:04 +0300

    Client (Asp.Net Core Mvc) Test 1/2

--- Services/Discount/FreeCourse.Services.Discount/Program.cs
+++ Services/Discount/FreeCourse.Services.Discount/Program.cs
@@ -13,6 +13,7 @@ namespace FreeCourse.Services.Discount
     {
         public static void Main(string[] args)
         {
+            
             CreateHostBuilder(args).Build().Run();
         }
 
--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -1,5 +1,8 @@
+using FreeCourse.Services.Order.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,16 @@ namespace FreeCourse.Services.Order.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var orderDbContext = serviceProvider.GetRequiredService<OrderDbContext>();
+                orderDbContext.Database.Migrate();
+
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
