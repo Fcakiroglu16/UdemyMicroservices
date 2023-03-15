b7d1f090fc8e0dd99121f41c533a07b04b0f1bd0
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2023-03-15 03:17:36 +0300

    Reference projeler kaldırıldı.

--- ExampleReferenceWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ExampleReferenceWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
